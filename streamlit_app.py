2572dfc1c1d76f5a76b7a44ffc1a0cb0c2509264
Remybeccat <115540114+Remybeccat@users.noreply.github.com>
2025-03-05 17:42:23 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,8 @@ def extract_time_spent(code_source):
         phase_name = phase.find('a', class_='discreet').text.strip()
         duration_text = phase.find('div', class_='tooltip-info-button')['title']
         st.write(duration_text)
-        CP_text = match = re.search(r'\[(.*?)\]', duration_text).replace('[', '').replace(']', '')
+        CP_text = re.search(r'\[(.*?)\]', duration_text)
+        CP_text = CP_text.replace('[', '').replace(']', '')
         st.write(CP_text)
         duration_match = re.search(r'(\d+,\d+|\d+)\s+j.?', duration_text)
         st.write(duration_match)
